649ba63a40856684c1fff01cb5bf4f65804a14fe
Abuzar <abuzarrrakhmonov@gmail.com>
2025-12-25 22:08:45 +0500

    get all low-stock product endpoint added

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,10 +1,18 @@
 import { Router } from "express";
 const router = Router();
-import { createNewProduct, getAllProducts, getProductById, updateProductById, deleteProductById } from "../controllers/products.controller.js";
+import {
+  createNewProduct,
+  getAllProducts,
+  getProductById,
+  updateProductById,
+  deleteProductById,
+  getAllLowStockProducts,
+} from "../controllers/products.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
 
 router.use(protectRoute);
 
+router.get("/low-stock", getAllLowStockProducts);
 router.post("/", createNewProduct);
 router.get("/", getAllProducts);
 router.get("/:id", getProductById);
