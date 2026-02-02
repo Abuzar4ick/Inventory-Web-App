24437d49a4c9ab334e0327a0bc62dca288cba03b
Abuzar <abuzarrrakhmonov@gmail.com>
2026-02-02 22:27:58 +0500

    Enhance authentication routes and CORS configuration for improved security and functionality

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -3,10 +3,10 @@ import dotenv from "dotenv";
 dotenv.config({ quiet: true });
 
 export const ENV = {
-    NODE_ENV: process.env.NODE_ENV || "development",
-    PORT: process.env.PORT || 3000,
-    DATABASE_URL: process.env.DATABASE_URL,
-    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
-    JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
-}
+  NODE_ENV: process.env.NODE_ENV || "development",
+  PORT: process.env.PORT || 3000,
+  DATABASE_URL: process.env.DATABASE_URL,
+  FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
+  JWT_SECRET: process.env.JWT_SECRET,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+};
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,13 +1,19 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 const router = Router();
 import {
   signupUser,
   loginUser,
   logoutUser,
 } from "../controllers/authController";
+import { protectRoute } from "../middlewares/authMiddleware";
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+
+// Protected route to check if user is authenticated
+router.post("/check", protectRoute, (req: Request, res: Response) =>
+  res.status(200).json(req.user),
+);
 
 export default router;
